d0817497b510d8ffe38057350b8e340b9612e3d8
venkybhuvanasi <venkat.b8659@gmail.com>
2023-01-03 12:18:27 +0530

    initial commit , project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { PracticeComponent } from './practice/practice.component';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PracticeComponent
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
