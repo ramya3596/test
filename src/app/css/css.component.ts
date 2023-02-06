27994af7a0cc260ca4e07caf809b6a4908ce95c2
venkybhuvanasi <venkat.b8659@gmail.com>
2023-02-06 21:29:02 +0530

    css changes

--- src/app/css/css.component.ts
+++ src/app/css/css.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-css',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./css.component.css']
 })
 export class CSSComponent {
+  @Output() 
+  myOutput:EventEmitter<string>= new EventEmitter(); 
 
+data:string = '0'
+
+// htags = ['h1','h2','h3','h4','h5','h6']
+
+topic : number = 0
+
+// activeHtag = 0
+
+sendValues(){  
+
+  console.log(this.data)
+  this.myOutput.emit(this.data);  
+}  
+
+colorTopic(){
+  this.topic = 1
+}
+
+// htagEffect(val:any){
+//   this.activeHtag = val
+// }
+
+bgcolorTopic(){
+  this.topic = 2
+}
 }
